767beafe475e71ba49b3b76611812633c2c91665
gunhee27 <gunhee27@naver.com>
2024-02-13 17:57:20 +0900

    20240213 first commit

---
+++ src/main/java/Geonhee/project/goodmorningbackend/GoodMorningBackendApplication.java
@@ -0,0 +1,13 @@
+package Geonhee.project.goodmorningbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodMorningBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoodMorningBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Geonhee/project/goodmorningbackend/GoodMorningBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package Geonhee.project.goodmorningbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoodMorningBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
